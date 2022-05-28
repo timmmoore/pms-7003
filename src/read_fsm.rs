38a60b84ed00c612abf82d84e779b0a58a3503d0
Tim Moore <tim@youngmoores.com>
2022-05-28 14:36:18 -0700

    test

--- src/read_fsm.rs
+++ src/read_fsm.rs
@@ -43,7 +43,8 @@ impl<'a> ReadStateMachine<'a> {
 
     fn retry(&mut self) {
         if self.retries == 0 {
-            self.state = State::Failed;
+            self.retries = 10;
+            //self.state = State::Failed;
         } else {
             self.retries -= 1;
 		let mut delayt = self.timer.count_down();
